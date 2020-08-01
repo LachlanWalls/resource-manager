03139b49c2f392f293053c75ec58d7bf00cfe5cb
Lachlan Walls <lachlan@Lachlans-MBP.modem>
2020-08-01 17:20:35 +1000

    Add mariadb connection

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
 
+const mariadb = require('mariadb')
+let conn = null
+
+const config = require('./config.json')
+
 app.get('/', (req, res) => res.send('Hello, world!'))
 
-app.listen(8080, () => console.log('resource-manager listening on localhost:8080'))
+app.get('/test', async(req, res) => {
+    res.send('ok')
+    let results = await conn.query('SELECT * FROM users;')
+    console.log(results)
+})
+
+mariadb.createConnection(config.mariaoptions).then(con => {
+
+    conn = con
+    app.listen(8080, () => console.log('resource-manager listening on localhost:8080'))
+
+}).catch(console.error)
+
